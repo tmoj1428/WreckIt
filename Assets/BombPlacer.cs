f0df66855232202820e7bae3d53fb061139b4735
mohammad javad taheri <tmoj.1428@gmail.com>
2021-11-04 20:13:53 +0330

    Add bomb prefab

---
+++ Assets/BombPlacer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPlacer : MonoBehaviour
+{
+    public GameObject bombPrefab;
+    public GameObject player;
+    public float timerMaxTime;
+
+    private float currentTimeVal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTimeVal = timerMaxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTimeVal > 0)
+        {
+            currentTimeVal -= Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GameObject bomb = Instantiate(bombPrefab);
+                bomb.transform.position = player.transform.position;
+                
+                currentTimeVal = timerMaxTime;
+            }
+        }
+    }
+}
