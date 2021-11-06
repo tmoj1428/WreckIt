5a2f0144d5c3503121e2d9834cb3614f08476f17
mohammad javad taheri <tmoj.1428@gmail.com>
2021-11-06 22:47:59 +0330

    Add box and bomb destruction on collision

---
+++ Assets/BoxController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("BombObj"))
+        {
+            Debug.Log("Destroooooy!!!!!!!!!");
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
