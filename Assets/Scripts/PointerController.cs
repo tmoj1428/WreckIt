ad30e45f8f052e31a606cf9531cf462277feda43
mohammad javad taheri <tmoj.1428@gmail.com>
2021-11-17 12:08:23 +0330

    Add intro scene to game

---
+++ Assets/Scripts/PointerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PointerController : MonoBehaviour
+{
+    public GameObject playText;
+    public GameObject quitText;
+
+    private bool onPlayButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(-8, 0.5f, 0);
+        onPlayButton = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onPlayButton)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                onPlayButton = false;
+                transform.position = new Vector3(-8, -2.5f, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                //Debug.Log("Play pressed");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                onPlayButton = true;
+                transform.position = new Vector3(-8, 0.5f, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
